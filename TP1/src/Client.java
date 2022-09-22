a74985887b500239d25a8ff46bd001faa5847794
Tommy Tremblay <2130081@collegebart.ca>
2022-09-22 16:29:48 -0400

    start base client

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -1,2 +1,42 @@
+import java.net.*;
+import java.io.*;
+
 public class Client {
+    private Socket socket = null;
+    private DataInputStream in = null;
+    private  DataOutputStream out = null;
+
+    public Client(String address, int port){
+        try{
+            socket = new Socket(address, port);
+            System.out.println("Connecter");
+
+            in = new DataInputStream(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
+
+            DataInputStream In = new DataInputStream(System.in);
+
+            String line = "";
+            while(!line.equals("fini")){
+                try{
+                    line = in.readLine();
+                    out.writeUTF(line);
+                }
+                catch (IOException i){
+                    System.out.println(i);
+                }
+            }
+            try{
+                in.close();
+                out.close();
+                socket.close();
+            }
+            catch (IOException e){
+                System.out.println(e);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,2 +1,14 @@
+import java.net.*;
+import java.io.*;
+
 public class Main {
+    public static void main(String[] argv){
+        try{
+            InetAddress ip = InetAddress.getByName("jmarshall.com");
+            Client c = new Client(ip.getHostAddress(),80);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
 }

c83ff3bdd25e3c043b42d0ff460656e3204a0e9b
Tommy Tremblay <2130081@collegebart.ca>
2022-09-29 16:16:36 -0400

    get method

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -6,6 +6,9 @@ public class Client {
     private DataInputStream in = null;
     private  DataOutputStream out = null;
 
+    private BufferedReader reader = null;
+    private  PrintWriter writer = null;
+
     public Client(String address, int port){
         try{
             socket = new Socket(address, port);
@@ -14,13 +17,31 @@ public class Client {
             in = new DataInputStream(System.in);
             out = new DataOutputStream(socket.getOutputStream());
 
-            DataInputStream In = new DataInputStream(System.in);
+
+
+
+//            writer = new PrintWriter(socket.getOutputStream());
+//            writer.print("GET / HTTP/1.0\r\n");
+//            writer.print("HOST: robdangero.us\r\n\r\n");
+//            writer.flush();
+//            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//            String outString;
+//
+//            while ((outString = reader.readLine())!=null){
+//                System.out.println(outString);
+//            }
 
             String line = "";
             while(!line.equals("fini")){
                 try{
+
+
                     line = in.readLine();
-                    out.writeUTF(line);
+//                    out.writeUTF(line);
+
+                    CommandReceive(line);
+
                 }
                 catch (IOException i){
                     System.out.println(i);
@@ -30,6 +51,8 @@ public class Client {
                 in.close();
                 out.close();
                 socket.close();
+                writer.close();
+                reader.close();
             }
             catch (IOException e){
                 System.out.println(e);
@@ -37,6 +60,31 @@ public class Client {
         }
         catch (Exception e){
             System.out.println(e);
+        }
+    }
+
+
+    private void CommandReceive(String line){
+        switch (line){
+            case "GET":
+                try{
+                    writer = new PrintWriter(socket.getOutputStream());
+                    writer.print("GET / HTTP/1.0\r\n");
+                    writer.print("HOST: robdangero.us\r\n\r\n");
+                    writer.flush();
+                    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    String outString;
+
+                    while ((outString = reader.readLine())!=null){
+                        System.out.println(outString);
+                    }
+                }
+                catch (IOException e){
+                    System.out.println(e);
+                }
+                break;
+            default:
         }
     }
 }
--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] argv){
         try{
-            InetAddress ip = InetAddress.getByName("jmarshall.com");
+            InetAddress ip = InetAddress.getByName("robdangero.us");
             Client c = new Client(ip.getHostAddress(),80);
         }
         catch(IOException e){
